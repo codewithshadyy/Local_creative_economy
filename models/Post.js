b2726e3e796435c295cf220ee1284f49ffa1221d
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-04-24 22:16:22 +0300

    Created post model

---
+++ models/Post.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose")
+const { string } = require("zod")
+
+const postSchema = new mongoose.Schema({
+    author:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Creator"
+    },
+    content:{
+        type:string,
+        
+    },
+    image:{
+        type:string
+        
+    },
+    likes:[
+        {
+            type:mongoose.Schema.Types.ObjectId
+        }
+    ]
+})
