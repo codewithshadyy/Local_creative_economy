df93220c94a8c974719a0d9786fb3664b569adb4
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-04-24 11:45:47 +0300

    Implemented creator model

---
+++ models/Creator.js
@@ -0,0 +1,46 @@
+
+const mongoose = require("mongoose")
+const { validator } = require("validator.js")
+
+
+const creatorSchema = new mongoose({
+    username:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    email : {
+        type:String,
+        required:true,
+        unique:true,
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please provide a valid email']
+    },
+
+    password:{
+        type:String,
+        required:true,
+        
+    },
+    profilePicture: { 
+        type: String,
+         default: "" 
+        },
+    followers: {
+         type: Array,
+          default: []
+         },
+    following: {
+         type: Array,
+          default: []
+         },
+    isAdmin: { 
+        type: Boolean,
+         default: false 
+        },
+
+
+})
+
+const creator = mongoose.model("Creator", creatorSchema)
+
+module.exports = creator
