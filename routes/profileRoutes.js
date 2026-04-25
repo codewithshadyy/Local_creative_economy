d039cafead7faecea437276ad235b79e9df12bbe
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-04-25 18:24:23 +0300

    Routed the update post endpoint

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -2,12 +2,13 @@ const express = require("express")
 const router = express.Router()
 
 const {protect} = require("../middlewares/protect")
-const {createProfile, getMyProfile, getAnyUserProfile} = require("../controllers/profileController")
+const {createProfile, getMyProfile, getAnyUserProfile, updateProfile} = require("../controllers/profileController")
 
 
 router.post("/create", protect, createProfile)
 router.get("/me",  protect, getMyProfile)
 router.get("/:creatorId", protect, getAnyUserProfile)
+router.put("/update/:id", protect, updateProfile)
 
 
 
