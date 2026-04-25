d3b5b94d59466cb41dddf3673bbd24ed86658179
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-04-25 17:31:31 +0300

    Build create profile route with protected route

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -1,2 +1,10 @@
 const express = require("express")
 const router = express.Router()
+
+const {protect} = require("../middlewares/protect")
+const {createProfile} = require("../controllers/profileController")
+
+
+router.post("/create/profile", protect, createProfile)
+
+
