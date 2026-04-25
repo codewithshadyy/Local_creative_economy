d6628847267df6836adc12cc7369842fecfad2e4
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-04-25 17:45:16 +0300

    Routed the getProfile endpoint

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -2,10 +2,11 @@ const express = require("express")
 const router = express.Router()
 
 const {protect} = require("../middlewares/protect")
-const {createProfile} = require("../controllers/profileController")
+const {createProfile, getProfile} = require("../controllers/profileController")
 
 
 router.post("/create", protect, createProfile)
+router.get("/see",  protect, getProfile)
 
 
 
