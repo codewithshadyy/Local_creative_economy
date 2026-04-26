d765ecb03539a0ed2f03d9811bfeb86e015fa4af
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-04-26 16:20:40 +0300

    Routed deletePost endpoint

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,12 +1,13 @@
 const express = require("express")
 const router = express.Router()
-const {createPost, getAllPost, getSinglePost, updatePost} = require("../controllers/postController")
+const {createPost, getAllPost, getSinglePost, updatePost, deletePost} = require("../controllers/postController")
 const {protect} = require("../middlewares/protect")
 
 router.post("/create",protect, createPost)
 router.get("/getPosts/:id", protect, getSinglePost)
 router.get("/getPosts", protect, getAllPost)
 router.put("/updatePosts/:id", protect,updatePost )
+router.delete("/deletePost/:id", protect, deletePost)
 
 
 
