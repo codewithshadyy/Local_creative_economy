59413d5e4774271cc4117c8d8c30f9e61f49602d
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-04-26 15:57:41 +0300

    Routed updatePost endpoint

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express")
 const router = express.Router()
-const {createPost, getAllPost, getSinglePost} = require("../controllers/postController")
+const {createPost, getAllPost, getSinglePost, updatePost} = require("../controllers/postController")
 const {protect} = require("../middlewares/protect")
 
 router.post("/create",protect, createPost)
 router.get("/getPosts/:id", protect, getSinglePost)
 router.get("/getPosts", protect, getAllPost)
+router.put("/update/:id", protect,updatePost )
 
 
 
