77301d7b510e9c94657e9578c1aecc0c0bcc3b1e
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-04-26 15:32:58 +0300

    Routed get single and get multiple post endpoints

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,9 +1,11 @@
 const express = require("express")
 const router = express.Router()
-const {createPost} = require("../controllers/postController")
+const {createPost, getAllPost, getSinglePost} = require("../controllers/postController")
 const {protect} = require("../middlewares/protect")
 
 router.post("/create",protect, createPost)
+router.get("/posts/:id", protect, getSinglePost)
+router.get("/posts", protect, getAllPost)
 
 
 
