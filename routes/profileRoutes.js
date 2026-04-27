b654bd5d41e876e5aab0a68713ef2e4f158c12a9
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-04-27 19:29:51 +0300

    Routing follow endpoint

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -2,13 +2,14 @@ const express = require("express")
 const router = express.Router()
 
 const {protect} = require("../middlewares/protect")
-const {createProfile, getMyProfile, getAnyUserProfile, updateProfile} = require("../controllers/profileController")
+const {createProfile, getMyProfile, getAnyUserProfile, updateProfile, followCreator} = require("../controllers/profileController")
 
 
 router.post("/create", protect, createProfile)
 router.get("/me",  protect, getMyProfile)
 router.get("/:creatorId", protect, getAnyUserProfile)
 router.put("/update/:id", protect, updateProfile)
+router.put("/follow/:id", protect, followCreator)
 
 
 
