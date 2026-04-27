7960c518142da4f225c5b55369fbe51f04bef43e
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-04-27 17:22:12 +0300

    Routed pos reply endpoint

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const router = express.Router()
-const {createPost, getAllPost, getSinglePost, updatePost, deletePost} = require("../controllers/postController")
+const {createPost, getAllPost, getSinglePost, updatePost, deletePost, replyPost} = require("../controllers/postController")
 const {protect} = require("../middlewares/protect")
 
 router.post("/create",protect, createPost)
@@ -8,6 +8,8 @@ router.get("/getPosts/:id", protect, getSinglePost)
 router.get("/getPosts", protect, getAllPost)
 router.put("/updatePosts/:id", protect,updatePost )
 router.delete("/deletePost/:id", protect, deletePost)
+router.post("/reply", protect, replyPost)
+
 
 
 
