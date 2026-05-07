15f47d97d0bd83cac1b4d4aea2a744cfc37da453
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-05-08 00:49:55 +0300

    .

--- app.js
+++ app.js
@@ -76,6 +76,26 @@ app.use("/api/v1/profiles", profileRoutes)
 app.use("/api/v1/feeds", tlRoutes)
 
 
+// getting lgs route
+app.get("/api/logs", async (req, res) => {
+  try {
+    const logsPath = path.join(logsDir, "access.log");
+
+    const logs = fs.readFileSync(logsPath, "utf8");
+
+    res.status(200).json({
+      success: true,
+      logs,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+});
+
+
 
 
 
