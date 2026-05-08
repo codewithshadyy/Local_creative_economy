9c89499f8985890cb4d802ceb856ba849d970176
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-05-08 20:41:48 +0300

    Routed deleteLogs

--- routes/logsRoute.js
+++ routes/logsRoute.js
@@ -1,9 +1,10 @@
 const express = require("express")
 const router = express.Router()
-const { getLogs } = require("../controllers/logsController")
+const { getLogs, deleteLogs } = require("../controllers/logsController")
 
 
 router.get("/logs", getLogs)
+router.delete("delete/logs", deleteLogs)
 
 
 module.exports = router
