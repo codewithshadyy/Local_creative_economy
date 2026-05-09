4cf9769ff8f310b0d3cfea63e9455023ff2ae079
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-05-09 22:13:23 +0300

    .....

--- routes/creatorRoutes.js
+++ routes/creatorRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const router = express.Router()
 const loginLimiter = require("../security/loginRateLimit")
-const {register, login, findCreators} = require("../controllers/creatorController")
+const {register, login, refresh, findCreators} = require("../controllers/creatorController")
 const {protect} = require("../middlewares/protect")
 
 
@@ -9,7 +9,7 @@ const {protect} = require("../middlewares/protect")
 router.post("/creator/register", register)
 router.post("/creator/login", loginLimiter, login)
 router.get("/creators",protect, findCreators)
-
+router.post("/refresh",  refresh)
 
 
 module.exports = router
