b80389091a0150b3a9de300d6fbaef126a1d9ba3
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-05-09 23:01:42 +0300

    ........fixing ownership middleware

--- middlewares/isOwner.js
+++ middlewares/isOwner.js
@@ -1,12 +1,27 @@
 
-const creator = require("../models/Creator")
+const Creator = require("../models/Creator")
+const Post=require("../models/Post")
 
 exports.isOwner = async (req,res,next) => {
 
-    if(req.creator && req.creator.id){
-        next
-    }else{
-        res.status(400).json({message:"Can't delete this post"})
+   try {
+
+     const creatorId  = req.Creator?.id
+    const postOwner = req.Post?.Creator
+
+    if (!creatorId) {
+        return res.status(401).json({ message: "Not authenticated" });
     }
+
+    if(postOwner.toString() !== creatorId.toString()){
+        return res.status(403).json({ message: "Not allowed to modify this post" });
+    }
+
+    next()
+    
+   } catch (error) {
+    res.status(500).json({message:error.message})
+    
+   }
     
 }
