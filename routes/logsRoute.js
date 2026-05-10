961d1bafeb943a4d1c30218d1dad516f220e9e11
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-05-10 10:11:59 +0300

    Protecting logs rroute to only admin

--- routes/logsRoute.js
+++ routes/logsRoute.js
@@ -1,10 +1,14 @@
 const express = require("express")
 const router = express.Router()
 const { getLogs, deleteLogs } = require("../controllers/logsController")
+const {protect} = require("../middlewares/protect")
+const {adminOnly} = require("../middlewares/adminMiddleware")
 
 
-router.get("/logs", getLogs)
-router.delete("/delete/logs", deleteLogs)
+
+
+router.get("/logs",protect,adminOnly, getLogs)
+router.delete("/delete/logs", protect, adminOnly, deleteLogs)
 
 
 module.exports = router
