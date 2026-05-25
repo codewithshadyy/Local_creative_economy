417b934848c708f80e6373edabcbaca0f3ebce64
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-05-25 23:38:17 +0300

    Removing payment Intergration via remove-payment branch

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const tlRoutes = require("./routes/tlRoutes")
 const swaggerDoc = require("./documentation/docs")
 const swaggerUi = require('swagger-ui-express');
 const logRoutes = require("./routes/logsRoute")
-const paymentRoutes = require("./routes/paymentRoutes")
+
 
 
 
@@ -82,7 +82,7 @@ app.use("/api/v1/admin", logRoutes)
 app.use("/api/v1/posts", postRoutes)
 app.use("/api/v1/profiles", profileRoutes)
 app.use("/api/v1/feeds", tlRoutes)
-app.use("/api/v1/pay", paymentRoutes)
+
 
 
 
